3e15f68823e48e2e2bb9657ca56316c608c001c0
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-04 17:40:39 +0900

    xammp PHP　変数

---
+++ lesson.php
@@ -0,0 +1,9 @@
+<?php
+$name = "三浦";
+
+echo ($name) . "<br>";
+
+echo ($name . "さんこんにちは") . "<br>";
+echo ($name . "さんこんばんは") . "<br>";
+echo ($name . "さんおはよう") . "<br>";
+echo ($name . "さんありがとう") . "<br>";
