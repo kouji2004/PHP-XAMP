6701f2fd2752f9f3bee75ca10970afdfbd32e4d1
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-04 17:59:31 +0900

    xampp 演算子

---
+++ lesson3.php
@@ -0,0 +1,21 @@
+<?php
+$number = 10 + 60 / 3;
+$num1 = 20;
+
+$total = $num1 + $number;
+echo ($number) . "<br>";
+
+echo ($total) . "<br>";
+
+//実行結果　
+//30
+//50
+
+
+
+
+$num3 = 10;
+$num3++;
+echo ($num3);
+//実行結果　
+//11
