d5a5359bb0c5efce6fb5d1aa063283c4297a95a8
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-05 22:23:30 +0900

    ループ処理(foreach)　現場でよく使われる

--- PHP2/lesson4.php
+++ PHP2/lesson4.php
@@ -1,6 +1,25 @@
 <?php
-$list =["a","b","c"];
+$list = ["a", "b", "c"];
 
-foreach($list as $value){
+foreach ($list as $value) {
   echo $value;
 }
+
+//実行結果
+//abc
+
+//構造(valueの所がそれぞれの配列（a,b,c）に置き換わっている)
+//step1
+//foreach([a,b,c]as a){
+  //aを使った処理
+//}
+
+//step2
+//foreach([a,b,c]as b){
+  //bを使った処理
+//}
+
+//step3
+//foreach([a,b,c]as c){
+  //cを使った処理
+//}
