28c0bd2d9ce138ee492ea3129e2f8cbe19fb96db
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-05 22:13:04 +0900

    配列とは、複数の値をまとめることができる

---
+++ PHP2/lesson2.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  $name1 = "三浦";
+  $text = "PHPの変数の勉強";
+
+  echo $name1 . "は" . $text . "をしています" . "<br>";
+  ?>
+
+  <?php
+  $users = ["三浦", "配列"];
+  $users[0];
+  $users[1];
+
+  echo $users[0] . "は" . $users[1] . "の勉強をしている"
+
+  ?>
+
+</body>
+
+</html>
