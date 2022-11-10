5ced858ca6aecab6e376d452f0104aa5f9554acd
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-10 23:38:48 +0900

    require_once

--- require/lesson5.php
+++ require/lesson5.php
@@ -3,3 +3,26 @@
 require("in.php");
 require("in.php");
 require("in.php");
+?>
+
+<h1>/////////////////////////</h1>
+
+<?php
+// インクルードファイルがない場合
+require("in.php");
+echo "ファイルないよ\n";
+?>
+
+<h1>/////////////////////////</h1>
+
+<?php
+// ファイルの取り込み
+require_once("in.php");
+require_once("in.php");
+require_once("in.php");
+?>
+
+<p>既にrequireが使われているため表示されない。もしrequireが使われていなければ
+  実行結果は
+  require と require_once のサンプルスクリプトだよ。の記述が一つだけ表示される。
+</p>
