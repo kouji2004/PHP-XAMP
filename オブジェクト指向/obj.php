fd07c04ee649f416c426d17f3b63634ac46ac1c4
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-11 00:18:30 +0900

    設計図のまま使う方法

--- オブジェクト指向/obj.php
+++ オブジェクト指向/obj.php
@@ -6,9 +6,11 @@ class character
 {
   public $hitpoint;
 
-  function hit()
+
+
+  function hit($point)
   {
-    echo "攻撃した!<br>";
+    echo "攻撃した!モンスターに" . $point . "のダメージ";
   }
 
   function runAway()
@@ -26,5 +28,11 @@ class character
 $hero = new character();
 $hero->hitpoint = 20;
 
-// print_r($hero);
-// echo "<br>";
+print_r($hero);
+echo "<br>";
+$hero->hit(5);
+
+//設計図のまま使う方法
+//クラス名::メソッド名
+// クラスメソッド名の前にstatic
+// character::hit(5);
