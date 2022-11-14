e4e419a27c90ac0d89f5192984312e3336608d9b
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-14 13:41:54 +0900

    PHPの絵本　データの受け渡し

---
+++ PHPの絵本/基本的なプログラム/lesson.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>データの受け渡し</title>
+</head>
+
+<body>
+  <form action="receivename.php" method="POST">
+    <p>名前:<input type="text" name="name"></p>
+    <p><input type="submit" value="送信"></p>
+  </form>
+</body>
+
+</html>
---
+++ PHPの絵本/基本的なプログラム/receivename.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>データの受け渡し</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    print($_POST["name"]);
+    ?>
+    さん、こんにちは
+  </p>
+
+</body>
+
+</html>
