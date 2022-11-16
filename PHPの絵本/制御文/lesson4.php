0833ea022907c050370989af95ebf13a3cff4744
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-16 15:04:58 +0900

    while文　と  do~while文

--- PHPの絵本/制御文/lesson4.php
+++ PHPの絵本/制御文/lesson4.php
@@ -4,5 +4,14 @@ while ($a < 5) {
   print "count" . $a++ . "<br>";
 }
 ?>
+<br>
 
 <!-- do~while -->
+<?php
+$s = $i = 0;
+do {
+  $i = ++$i;
+  $s = $s + $i;
+} while ($i < 10);
+print "1から" . $i . "　までの和は" . $s;
+?>
