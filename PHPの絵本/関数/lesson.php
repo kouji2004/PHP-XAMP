bd2a5e13666b01dd6506a03b3c3dd294f6de9b8b
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-16 15:55:35 +0900

    関数の定義

---
+++ PHPの絵本/関数/lesson.php
@@ -0,0 +1,25 @@
+<!-- 関数 -->
+<?php
+function abc($a)
+{
+  print "引数の値は $a";
+  return;
+  //実行結果:引数の値は 
+}
+abc($a)
+
+// 予備知識
+// function→関数の書き始め
+?>
+<br>
+
+
+<?php
+function hello()
+{
+  print "hello ";
+  //実行結果:hello 
+}
+echo hello()
+?>
+
