e87e1e991654f8b86e47b84c6f9913a2dfe59a91
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-16 14:52:17 +0900

    foreach

--- PHPの絵本/制御文/lesson3.php
+++ PHPの絵本/制御文/lesson3.php
@@ -4,3 +4,14 @@ $a = array(1, 2, 3);
 foreach ($a as $key => $b) {
   print "$key<br>\n";
 }
+?>
+<br>
+
+<!-- 例題1 -->
+
+<?php
+$siki = array("spr" => "春", "sum" => "夏", "aut" => "秋", "win" => "冬",);
+foreach ($siki as $key => $b) {
+  print "$key は $b<br>\n";
+}
+?>
