2b66d078b801bab38567b215c63ee3c631b1c4cb
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-16 13:26:22 +0900

    演算子

---
+++ PHPの絵本/演算子/lesson.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>演算子</title>
+</head>
+
+<body>
+  <?php
+  $a = 5 + 5;
+  $b = 5 * 5;
+  $c = 5 - 6;
+  $d = 5 / 5;
+
+  print "5+5は $a<br>\n";
+  print "5*5は $b<br>\n";
+  print "5-6は $c<br>\n";
+  print "5/5は $d<br>\n";
+  ?>
+</body>
+
+</html>
