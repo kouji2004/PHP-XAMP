5792aac3c9096c83b3716a6e5c690c0189a1af6e
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-17 14:38:22 +0900

    ファイルのアップロード

---
+++ PHPの絵本/インターネットとの連携/lesson3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <form action="lesson3-3.php" method="POST" enctype="multipart/form-data">
+    <p><input type="file" name="upfile"></p>
+    <p><input type="submit" value="アップロード"></p>
+  </form>
+</body>
+
+</html>
